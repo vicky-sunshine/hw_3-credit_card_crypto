22473a9040fe4f565aa3c1ae65dc27fc3b9fd3bf
vicky <lch82327@gmail.com>
2016-03-09 13:27:41 +0800

    add mixin and init the credit card object

--- credit_card.rb
+++ credit_card.rb
@@ -1,31 +1,42 @@
 require_relative './luhn_validator.rb'
 require 'json'
+require 'openssl'
 
 class CreditCard
-  # TODO: mixin the LuhnValidator using an 'include' statement
-
+  # mixin the LuhnValidator using an 'include' statement
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
-    # TODO: initialize the instance variables listed above (do not forget the '@')
+    # initialize the instance variables listed above (do not forget the '@')
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
-     }.to_json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network }.to_json
   end
 
   # returns all card information as single string
   def to_s
-    self.to_json
+    to_json
   end
 
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
-    # TODO: deserializing a CreditCard object
+    card_json = JSON.load(card_s)
+    CreditCard.new(card_json['number'],
+                   card_json['expiration_date'],
+                   card_json['owner'],
+                   card_json['credit_network'])
   end
 
   # return a hash of the serialized credit card object
