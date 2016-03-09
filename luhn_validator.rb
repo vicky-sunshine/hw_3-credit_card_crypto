ceaf8ac3e92607481c9cd82687e15c7949ba0d7e
vicky <lch82327@gmail.com>
2016-03-09 13:27:05 +0800

    luhn algo implement

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -4,8 +4,10 @@ module LuhnValidator
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
   def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    nums_a = number.to_s.chars.map(&:to_i).reverse
+    double = nums_a.each_with_index.map { |e, i| i.even? ? e : e * 2 }
+    sum_digit = double.map { |e| e >= 10 ? e / 10 + e - 10 : e }
+    sum = sum_digit.reduce(:+)
+    sum % 10 == 0
   end
 end
