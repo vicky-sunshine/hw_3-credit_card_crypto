a8816aca56db653d1c62dfac7e55797e2d3a2ed9
vicky <lch82327@gmail.com>
2016-03-10 18:04:40 +0800

    use a loop to calculate weight of each num and sum all

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -5,13 +5,16 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i).reverse!
-    double = nums_a.each_with_index.map do |e, i|
-      if i.even? && e > 4
-        e * 2 - 9
+    sum = nums_a.each_with_index.reduce(0) do |acc, pair|
+      element, index = pair
+      if index.odd? && element > 4
+        acc + element * 2 - 9
+      elsif index.odd?
+        acc + element * 2
       else
-        i.even? ? e * 2 : e
+        acc + element
       end
     end
-    double.reduce(:+) % 10 == 0
+    sum % 10 == 0
   end
 end
